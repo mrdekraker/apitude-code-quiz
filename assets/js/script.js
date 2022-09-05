4e6c548f9602e4afb8f14ae8b461bd40c0b6b58e
mrdekraker <30578794+mrdekraker@users.noreply.github.com>
2022-09-05 00:49:37 -0500

    added randomize questions and looped answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,65 @@
-//  !DOM ELEMENTS
-const wrapper = document.querySelector(`.wrapper`);
+// !DOM ELEMENTS
 const landing = document.querySelector(`.landing`);
-const timerEl = document.querySelector(`.timer`);
-const startQuiz = document.querySelector(`#button`);
 const quiz = document.querySelector(`.quiz`);
-const quizButton = document.querySelector(`#button`);
+const startBtn = document.querySelector(`.startBtn`);
+// !QUESTION DISPLAYS
+const questionDisplay = document.querySelector(`.question-display`);
+const answers = Array.from(document.getElementsByClassName(`answer-items`));
+const answerList = document.querySelector(`.answer-list`);
+let currentQuestion = {};
+const acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+// !TIMER VARIABLES
+const timer = document.querySelector(`.timer`);
+const time = availableQuestions.length * 15;
 
-// !TIMER FUNCTION
-function countdown() {
-    let timeLeft = 120;
-    // timerEl.textContent = timeLeft;
+// !GAME MECHANICS
+const BONUS = 10;
+const MAX_QUESTIONS = 5;
+
+// GET NEW QUESTIONS
+function getNewQuestion() {
+    questionCounter += 1;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    questionDisplay.textContent = currentQuestion.question;
+
+    answers.forEach((answer, i) => {
+        // const number = answer.dataset.numbers;
+        const answerDisplay = answers[i].textContent;
+        answer.textContent = currentQuestion.answers[i];
+    });
+}
+
+function timerCountdown() {
+    let timeLeft = time;
 
     const timeInterval = setInterval(() => {
         if (timeLeft > 1) {
-            timerEl.textContent = `Time: ${timeLeft}s`;
+            timer.textContent = `Time: ${timeLeft}s`;
             timeLeft -= 1;
         } else if (timeLeft === 1) {
-            timerEl.textContent = `Time: ${timeLeft}s`;
+            timer.textContent = `Time: ${timeLeft}s`;
             timeLeft -= 1;
         } else {
-            timerEl.textContent = `Time is up!`;
+            timer.textContent = `Time is up!`;
             clearInterval(timeInterval);
         }
     }, 1000);
 }
 
-// !HIDE ELEMENTS
-// quiz.classList.add(`hide`);
-startQuiz.addEventListener(`click`, () => {
-    landing.classList.add(`hide`);
-    quiz.classList.remove(`hide`);
-    countdown();
-});
+function startGame() {
+    startBtn.addEventListener(`click`, () => {
+        landing.classList.add(`hide`);
+        quiz.classList.remove(`hide`);
+        timerCountdown();
+    });
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+}
+
+startGame();
